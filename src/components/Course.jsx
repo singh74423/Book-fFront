d71f69fb6ecfc5f12d3964f50ba27c321af459b7
Ujjwal Singh <singhujjwal744@gmail.com>
2025-12-23 03:14:43 +0530

    HHH

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,48 +1,48 @@
 import React, { useEffect, useState } from "react";
 import Cards from "./Cards";
 import axios from "axios";
-
 import { Link } from "react-router-dom";
+
 function Course() {
   const [book, setBook] = useState([]);
+
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("https://book-backend-cjs3.onrender.com/api/book");
-        console.log(res.data);
-        setBook(res.data);
+        const res = await axios.get(
+          "https://book-backend-cjs3.onrender.com/api/book"
+        );
+        setBook(res.data.books || res.data);
       } catch (error) {
         console.log(error);
       }
     };
     getBook();
   }, []);
+
   return (
     <>
-      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
-        <div className="mt-28 items-center justify-center text-center">
-          <h1 className="text-2xl  md:text-4xl">
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 text-center">
+          <h1 className="text-2xl md:text-4xl">
             We're delighted to have you{" "}
-            <span className="text-pink-500"> Here! :)</span>
+            <span className="text-pink-500">Here! :)</span>
           </h1>
+
           <p className="mt-12">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
-            assumenda? Repellendus, iste corrupti? Tempore laudantium
-            repellendus accusamus accusantium sed architecto odio, nisi expedita
-            quas quidem nesciunt debitis dolore non aspernatur praesentium
-            assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
-            animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
-            consequatur!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit...
           </p>
+
           <Link to="/">
             <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
               Back
             </button>
           </Link>
         </div>
-        <div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-14">
+
+        <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {book.map((item) => (
-            <Cards key={item.id} item={item} />
+            <Cards key={item._id} item={item} />
           ))}
         </div>
       </div>
