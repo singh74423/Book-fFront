b31497ce7d2878e0c9ed1001187062a7b48f37d5
Ujjwal Singh <singhujjwal744@gmail.com>
2025-12-23 22:47:36 +0530

    fix: tailwind v4 postcss plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {
-      animation: {
-        float: "float 3s ease-in-out infinite",
-      },
-      keyframes: {
-        float: {
-          "0%, 100%": { transform: "translateY(0px)" },
-          "50%": { transform: "translateY(-15px)" },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
